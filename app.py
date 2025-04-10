0e5bd7ce3de0ff54de75a7d076e919c20e708329
CotyW <cwoodsid@visa.com>
2025-04-10 11:16:53 -0500

    portupdate2

--- app.py
+++ app.py
@@ -183,4 +183,4 @@ with app.app_context():
 
 if __name__ == '__main__':
     debug_mode = os.environ.get('FLASK_ENV') == 'development'
-    app.run(host='0.0.0.0', port=5000, debug=debug_mode)
+    app.run(host='0.0.0.0', port=3000, debug=debug_mode)
